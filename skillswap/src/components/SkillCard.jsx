d847247e8ff76eeb9ee6dd4698dece2ae39cdac8
rishuu186 <rishuu1806@gmail.com>
2026-09-21 00:10:33 +0530

    Added useState Skill interest

--- skillswap/src/components/SkillCard.jsx
+++ skillswap/src/components/SkillCard.jsx
@@ -1,8 +1,30 @@
+import { useState } from "react";
+
 function SkillCard(props) {
+  const [interested, setInterested] = useState(false);
+  const [count, setCount] = useState(0);
+
+  function handleInterest() {
+    if (interested) {
+      setInterested(false);
+      setCount(count - 1);
+    } else {
+      setInterested(true);
+      setCount(count + 1);
+    }
+  }
+
   return (
-    <div>
+    <div className="skill-card">
       <h3>{props.skill}</h3>
-      <p>Learn this skill from another user.</p>
+
+      <p>Learn this skill from other students.</p>
+
+      <button onClick={handleInterest}>
+        {interested ? "✓ Interested" : "I'm Interested"}
+      </button>
+
+      <p>People interested: {count}</p>
     </div>
   );
 }

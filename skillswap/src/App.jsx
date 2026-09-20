65d02d11c32539e06ac7f324b6fac4310ae968d2
rishuu186 <rishuu1806@gmail.com>
2026-09-20 22:51:41 +0530

    Initial React Setup

---
+++ skillswap/src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div>
+      <h1>Welcome to SkillSwap</h1>
+      <p>Learn and share your skills.</p>
+      <button>Get Started</button>
+    </div>
+  );
+}
+
+export default App;
